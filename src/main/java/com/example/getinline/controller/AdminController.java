30740b611aabfe3adb6e8ce1c3d81aaf0afd9103
KBS <qudtjs3636@gmail.com>
2022-01-19 02:10:03 +0900

    Add #1 - show query result to view
    query 결과를 thymeleaf를 통해 보여줌.

--- src/main/java/com/example/getinline/controller/AdminController.java
+++ src/main/java/com/example/getinline/controller/AdminController.java
@@ -1,9 +1,15 @@
 package com.example.getinline.controller;
 
+import com.example.getinline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: kbs
@@ -12,9 +18,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 
+    //@RequestParam 을 안쓰는 이유. query가 optional하므로.
+    //아래처럼 ?하고 집어넣으면 된다.
+    //http://localhost:8080/admin/places?placeType=COMMON&address=%EC%84%B1%EB%82%A8%EC%8B%9C
     @GetMapping("/places")
-    public String adminPlaces() {
-        return "admin/places";
+    public ModelAndView adminPlaces(
+            PlaceType placeType,
+            String placeName,
+            String address
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/places/{placeId}")
